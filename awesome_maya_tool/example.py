5e8c3137cb38893ae9a3000ec9735fbdd4d26af0
Mischa Kolbe <mischakolbe@gmail.com>
2020-07-19 01:45:12 -0700

    Trying to fix error

--- awesome_maya_tool/example.py
+++ awesome_maya_tool/example.py
@@ -4,8 +4,21 @@
 """
 from maya import cmds
 
+# IMPORTS ---
+# Python imports
+import copy
+import itertools
+import numbers
+import os
+import re
+import sys
+
+# Third party imports
+from maya import cmds
+from maya.api import OpenMaya
 
 def make_a_cube(name="test"):
     cube = cmds.polyCube(name=name)
 
     return cube
+
--- tests/base.py
+++ tests/base.py
@@ -1,10 +1,15 @@
 """
 Unit test base class.
 """
+
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# IMPORTS
+# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+# Python imports
 from unittest import TestCase
 
-# Adding the path through this base module takes care of an odd
-# issue where the tests were run too early.
+# Adding the NodeCalculator path through this base module takes care of an odd
+# issue where the tests were run before the NodeCalculator was loaded.
 import os
 tests_path = os.path.dirname(os.path.realpath(__file__))
 noca_path = tests_path.rsplit(os.sep, 1)[0]
@@ -13,6 +18,7 @@ import sys
 if noca_path not in sys.path:
     sys.path.insert(0, noca_path)
 
+# Third party imports
 import maya.cmds as cmds
 
 
