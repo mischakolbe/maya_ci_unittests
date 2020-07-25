8d38d6e318fedb80e1feadf3fd8c75e16194fff9
Mischa Kolbe <24309085+mischakolbe@users.noreply.github.com>
2020-07-25 01:41:06 -0700

    This commit should fail.

---
+++ tests/test_bla.py
@@ -0,0 +1,16 @@
+"""Example unittests.
+
+Note:
+    Visit unittest documentation page for all assert options:
+    https://docs.python.org/2.7/library/unittest.html#module-unittest
+"""
+from base import MayaBaseTestCase
+
+import example_code
+
+
+class TestMakeTransform(MayaBaseTestCase):
+    def test_node_name(self):
+        """Test whether returned name is correct."""
+        name = "TestTransform"
+        self.assertEqual(example_code.make_transform(name=name), "bob")
